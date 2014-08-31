73f4b59b833047c848c6486068da0060837fd3d7
Luke Burns <luke@lukeburns.com>
2014-08-31 11:22:50 -0400

    remove util dependency

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-var util = require('util');
-
-module.exports = proxi
+module.exports = proxi;
 
 function proxi (proxy, obj, keys) {
   for (var key in obj) {
-    if (util.isFunction(obj[key]) && (!keys || keys.indexOf(key) != -1)) {
+    if (typeof obj[key] == 'function' && (!keys || keys.indexOf(key) != -1)) {
       proxy[key] = obj[key].bind(obj);
     }
   }
